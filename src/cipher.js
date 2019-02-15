7ff2503e8a3cf0d47221701eadb6714ef2a45d70
DayMateluna <d.mateluna.c@gmail.com>
2019-02-15 09:24:46 -0300

    cambio con cifrado y decifrado funcional

--- src/cipher.js
+++ src/cipher.js
@@ -6,19 +6,21 @@ window.cipher = {
 
   for(let i=0;i<text.length;i++)
   {  
-    ascii=text.charCodeAt(i);
+   ascii=text.charCodeAt(i);//con esto guarda el codigo ascci en cada iteracion
 
-    if(ascii>=65 && ascii<=90)
+   // if(ascii>=65 && ascii<=90)
     {
-      ascii1=(ascii-65+offset)%26+65;
+      ascii1=(ascii-65+offset)%26+65;//formula para obtener la posicion(numero) de la letra en nuestro abecedario
     }
 
-   result += String.fromCharCode(ascii1)
+   result += String.fromCharCode(ascii1);//trasforma el resultado de la formula en letra 
   }
 
   return result;
  
   },
+
+  
   decode: (offset,text) => {
     let ascii="";
   let ascii1="";
@@ -28,12 +30,12 @@ window.cipher = {
   {  
     ascii=text.charCodeAt(i);
 
-    if(ascii>=65 && ascii<=90)
+    //if(ascii>=65 && ascii<=90)
     {
       ascii1=(ascii-65-offset)%26+65;
     }
 
-   result += String.fromCharCode(ascii1)
+   result += String.fromCharCode(ascii1);
   }
 
   return result;
